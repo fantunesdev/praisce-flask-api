eeafb8720815f3278c39500576e115e2e2566dac
Fernando Antunes <f.antunes84@gmail.com>
2023-07-26 00:58:29 -0300

    Implementação dos métodos Update, Delete e Search

--- api/services/contato_services.py
+++ api/services/contato_services.py
@@ -17,3 +17,27 @@ def cadastrar_contato(contato):
 def listar_contatos():
     contatos = contato_model.Contato.query.all()
     return contatos
+
+
+def listar_contato_id(id):
+    contato = contato_model.Contato.query.filter_by(id=id).first()
+    return contato
+
+
+def listar_contato_nome(nome):
+    contatos = contato_model.Contato.query.filter(contato_model.Contato.nome.like(f'%{nome}%')).all()
+    return contatos
+
+
+def editar_contato(contato_db, contato_novo):
+    contato_db.nome = contato_novo.nome
+    contato_db.sobrenome = contato_novo.sobrenome
+    contato_db.email = contato_novo.email
+    contato_db.telefone = contato_novo.telefone
+    db.session.commit()
+    return contato_db
+
+
+def remover_contato(contato):
+    db.session.delete(contato)
+    db.session.commit()
--- api/views/contato_views.py
+++ api/views/contato_views.py
@@ -10,23 +10,69 @@ from ..services import contato_services
 class ContatoList(Resource):
     def get(self):
         contatos = contato_services.listar_contatos()
-        cs = contato_schema.ContatoSchema(many=True)
-        return make_response(cs.jsonify(contatos), 200)
+        schema_contato = contato_schema.ContatoSchema(many=True)
+        return make_response(schema_contato.jsonify(contatos), 200)
 
     def post(self):
-        cs = contato_schema.ContatoSchema()
-        validade = cs.validate(request.json)
+        schema_contato = contato_schema.ContatoSchema()
+        validade = schema_contato.validate(request.json)
         if validade:
             return make_response(jsonify(validade), 400)
         else:
             novo_contato = contato.Contato(
-                nome=request.json["nome"],
-                sobrenome=request.json["sobrenome"],
-                email=request.json["email"],
-                telefone=request.json["telefone"]
+                nome=request.json['nome'],
+                sobrenome=request.json['sobrenome'],
+                email=request.json['email'],
+                telefone=request.json['telefone']
             )
             result = contato_services.cadastrar_contato(novo_contato)
-            return make_response(cs.jsonify(result), 201)
+            return make_response(schema_contato.jsonify(result), 201)
+
+
+class ContatoDetail(Resource):
+    def get(self, id):
+        contato = contato_services.listar_contato_id(id)
+        if not contato:
+            return make_response(jsonify('Contato não encontrado.'), 404)
+        schema_contato = contato_schema.ContatoSchema()
+        return make_response(schema_contato.jsonify(contato), 200)
+
+    def put(self, id):
+        contato_db = contato_services.listar_contato_id(id)
+        if contato_db:
+            schema_contato = contato_schema.ContatoSchema()
+            validate = schema_contato.validate(request.json)
+            if validate:
+                return make_response(jsonify(validate), 400)
+            else:
+                novo_contato = contato.Contato(
+                    nome=request.json['nome'],
+                    sobrenome=request.json['sobrenome'],
+                    email=request.json['email'],
+                    telefone=request.json['telefone']
+                )
+                contato_editado = contato_services.editar_contato(contato_db, novo_contato)
+                return make_response(schema_contato.jsonify(contato_editado), 200)
+        return make_response(jsonify('Contato não encontrado.'), 404)
+
+    def delete(self, id):
+        contato_db = contato_services.listar_contato_id(id)
+        if contato:
+            contato_services.remover_contato(contato_db)
+            return make_response(jsonify(''), 204)
+        else:
+            return make_response(jsonify('Contato não encontrado.'), 404)
+
+
+class ContatoSearch(Resource):
+    def get(self, nome):
+        contato = contato_services.listar_contato_nome(nome)
+        if not contato:
+            return make_response(jsonify('Contato não encontrado.'), 404)
+        schema_contato = contato_schema.ContatoSchema(many=True)
+        return make_response(schema_contato.jsonify(contato), 200)
 
 
 api.add_resource(ContatoList, '/contatos')
+api.add_resource(ContatoDetail, '/contatos/<int:id>')
+api.add_resource(ContatoSearch, '/contatos/<string:nome>')
